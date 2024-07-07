81bd7f9071cb7f85916aab8c013b7c727ca5f129
Nur Akmal Jalil <48422687+nurakmaljalil91@users.noreply.github.com>
2024-07-07 19:06:26 +0800

    Add SDL 2

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,44 @@
 #include "components/Card.h"
 #include "components/Deck.h"
 #include "components/Player.h"
+#include "SDL2/SDL.h"
 
-int main() {
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Hello SDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+    SDL_RenderClear(ren);
+    SDL_RenderPresent(ren);
+
+    SDL_Delay(2000);
+
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return 0;
+}
+
+void SetupCardGame() {
     Deck deck;
 
     deck.initialize();
@@ -50,6 +86,4 @@ int main() {
 
     std::cout << player1.getName() << " has " << player1.getScore() << " points" << std::endl;
     std::cout << player2.getName() << " has " << player2.getScore() << " points" << std::endl;
-    
-    return 0;
 }
